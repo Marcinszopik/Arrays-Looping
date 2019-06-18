620df194b58fdf61ae6f2e08d65f95d496f071de
Marcinszopik <szopiks@yahoo.com>
2019-06-17 21:33:46 -0700

    Update scripts.js
    update the input user functionality when numbers 1, 2, and 3 are entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
-function myFunction() {
-  var answers = ["No Clue", "Beep", "Boop", "I'm Sorry Dave. I'm afraid I can't do that"];
-  var text = "";
-  var i;
-  for (i = 0; i < answers.length; i++) {
-    text += answers[i] + "<br>";
-  }
-  document.getElementById("demo").innerHTML = text;
-}
+var myAnswer = function(num, digit) {
+      if (num <= 1) {
+        return "Beep!";
+    } else if (num <= 2) {
+        return "Boop!";
+    } else if (num = 3) {
+      return "I'm sorry, Dave. I'm afraid I can't do that.";
+}};
+
+$(document).ready(function() {
+  $("form#my-Answer").submit(function(event) {
+    event.preventDefault();
+    var num = parseInt($("input#num").val());
+    var result = myAnswer(num);
+    $("#result").text(result);
+  });
+});
